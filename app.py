a53b08e15c083f61ebbf40e0c6443ab3aaaba0b8
shobhit043 <shobhit@HP-Notebook-480326ff.localdomain>
2024-01-31 16:38:53 +0530

    updated app and added mode_weights file

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
-from flask import Flask, request, jsonify
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["CUDA_VISIBLE_DEVICES"]="-1" 
+from flask import Flask, request, jsonify,render_template
 import numpy as np
 from keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import Dense,GlobalAveragePooling2D
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.models import Model
+from tensorflow.keras import Input
 from PIL import Image
 from io import BytesIO
 import pickle
 
+
 # Initialize Flask app
 app = Flask(__name__)
 
-# Load model and dictionary
-loaded_model = None
-index_to_class = None
+def create_model(num_classes):
+    base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
-def load_model_and_dictionary():
-    global loaded_model, index_to_class
-    if loaded_model is None:
-        loaded_model = tf.keras.models.load_model("final_model")
-    if index_to_class is None:
-        index_to_class = pickle.load(open('index_to_class.pkl', 'rb'))
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    inputs = Input(shape=(224, 224, 3))
+    x = base_model(inputs, training=False) 
+    x = GlobalAveragePooling2D()(x)
+    outputs = Dense(80, activation='softmax')(x) 
+
+    model = Model(inputs=inputs, outputs=outputs)
+
+    model.load_weights("model_weights.h5")
+
+    return model
+
 
 # Preprocess image
 def preprocess_image(img):
@@ -28,12 +42,28 @@ def preprocess_image(img):
     img_array = np.expand_dims(img_array, axis=0)
     return preprocess_input(img_array)
 
+
+# Model and Dictionary
+model = None
+index_to_class = None
+
+# Load dictionary 
+if index_to_class is None:
+    index_to_class = pickle.load(open('index_to_class.pkl', 'rb'))
+
+# Create model
+model=create_model(80)
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('index.html')
 # Define route for model prediction
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def predict():
 
     if 'image' not in request.files:
         return jsonify({'class': 'image not found'}) , 400
+        # render_template('result.html',{'class': 'image not found'})
     
     # Get image data from request
     image_file = request.files['image']
@@ -41,23 +71,22 @@ def predict():
     allowed_extensions = {'jpg', 'jpeg', 'png'}  # Add more if needed
     if image_file.filename.split('.')[-1].lower() not in allowed_extensions:
         return jsonify({'class': 'invalid image format'}) , 400
+        # render_template('result.html',{'class': 'invalid image format'})
     
     # Load image
     img = Image.open(BytesIO(image_file.read()))
     
-    # Preprocess image
+    # Preprocess imageEncoder
     img_array = preprocess_image(img)
 
-    # Load model and dictionary if not loaded
-    load_model_and_dictionary()
-    
     # Make predictions
-    predictions = loaded_model.predict(img_array)
+    predictions = model.predict(img_array)
     predicted_class_index = np.argmax(predictions)
     predicted_class = index_to_class[predicted_class_index]
     
     # Return prediction as JSON response
-    return jsonify({'class': predicted_class})
+    # return render_template('result.html',message = {'class': predicted_class})
+    return jsonify({'class':predicted_class})
 
 # Run the Flask app
 if __name__ == '__main__':
